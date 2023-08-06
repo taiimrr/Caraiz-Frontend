bc4f3dedd5e9eef8bb4e2d641d9c645f8657061d
taiimrr <taimoornasir590@gmail.com>
2023-08-06 17:46:01 +0500

    status page

---
+++ pages/platform_status/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ImageTitle from "../../components/imageTitle";
+import Meta from "../../components/Meta";
+import Image from "next/image";
+
+const Platform_status = () => {
+  return <div></div>;
+};
+
+export default Platform_status;
