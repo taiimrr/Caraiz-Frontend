0f80b4469914050ffd532aabf96c4efbab49c776
taiimrr <taimoornasir590@gmail.com>
2023-08-15 22:13:44 +0500

    forms

---
+++ pages/help_center/index.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+import ImageTitle from "../../components/imageTitle";
+import { help_center_data } from "../../data/help_center_data";
+import { accordion_data } from "../../data/help_center_data";
+import Accordion from "../../components/accordion";
+import Meta from "../../components/Meta";
+import Image from "next/image";
+
+const Help_center = () => {
+  const bgImage = "/images/page-title/knowledge_base_banner.jpg";
+  return (
+  
+  );
+};
+
+export default Help_center;
